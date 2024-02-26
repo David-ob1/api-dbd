e01b8a25e2dc3032ee8e2f7105b338a3df93df54
David-ob1 <davidvazquezt2001@gmail.com>
2024-02-25 21:53:50 -0300

    detalle controlador

---
+++ src/main/java/com/vazquez/dbdherencia/Controllers/PersonController.java
@@ -0,0 +1,36 @@
+package com.vazquez.dbdherencia.Controllers;
+
+import com.vazquez.dbdherencia.DTO.NewPerson;
+import com.vazquez.dbdherencia.Models.Person;
+import com.vazquez.dbdherencia.Repositories.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/persons")
+public class PersonController {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+
+    @PostMapping("")
+    public ResponseEntity<String> registerPerson(@RequestBody NewPerson newPerson){
+
+
+        Person person = new Person(newPerson.name(), newPerson.email(), newPerson.password());
+        personRepository.save(person);
+
+        return new ResponseEntity<>("Todo salio bien", HttpStatus.CREATED);
+
+    }
+
+
+    @GetMapping("")
+    public List<Person> getAll(){return personRepository.findAll()}
+
+}
