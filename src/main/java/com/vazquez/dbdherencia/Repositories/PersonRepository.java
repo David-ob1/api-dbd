b9815e2ffb2e2bcd1444d806e034478decacf104
David-ob1 <davidvazquezt2001@gmail.com>
2024-03-15 21:34:31 -0300

    @repository

--- src/main/java/com/vazquez/dbdherencia/Repositories/PersonRepository.java
+++ src/main/java/com/vazquez/dbdherencia/Repositories/PersonRepository.java
@@ -2,7 +2,9 @@ package com.vazquez.dbdherencia.Repositories;
 
 import com.vazquez.dbdherencia.Models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person,String> {
 
     Person findByEmail(String email);
