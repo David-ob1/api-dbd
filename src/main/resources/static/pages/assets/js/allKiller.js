c261e559eb279c6c4eb44b907921ce5f16f2f84a
David-ob1 <davidvazquezt2001@gmail.com>
2024-03-20 20:59:36 -0300

    tabla dinamica, survi dinamicos y codigo reutilizable

--- src/main/resources/static/pages/assets/js/allKiller.js
+++ src/main/resources/static/pages/assets/js/allKiller.js
@@ -4,18 +4,26 @@ const $containerCards = document.querySelector(".container-cards")
 console.log($containerCards.clientHeight)
 
 
-let allKiller
-let descriptionImg 
+let allCharacter,descriptionImg ,endpointGet
+let isSurvi = false
 
-let endpointGet = "/api/killers/all"
+let endpointKiller = "/api/killers/all"
+let endpointSurvi = "/api/survivor/all"
+
+
+
+let ruta = window.location.pathname;
+ ruta.toLocaleLowerCase().includes("killer") ? endpointGet ="/api/killers/all" : endpointGet = "/api/survivor/all"
+ isSurvi = endpointGet == "/api/survivor/all";
+
 fetch(endpointGet)
  .then(response => response.json())
  .then(data =>{
-     allKiller = data
-     console.log(allKiller)
+     allCharacter = data
+     console.log(allCharacter)
    
 
-     const template = generateTemplate (allKiller)
+     const template = generateTemplate (allCharacter)
      setCards(template,$containerCards)
 
  const cards = document.querySelectorAll(".card")
@@ -23,9 +31,14 @@ fetch(endpointGet)
  
  cards.forEach(card => {
      card.addEventListener("click", () => {
-         
          descriptionImg = `<img src="./assets/images/${card.id}.jpg" alt="" />`
+
+         let pjSelect = allCharacter.find(character => character.name == card.id)
+         
+        let tabla =  isSurvi ? generateTableSurvi(pjSelect) : generateTableKiller (pjSelect)
+
          containerDescription.innerHTML  = descriptionImg
+         containerDescription.innerHTML += tabla
          
         })
     })
@@ -37,14 +50,6 @@ fetch(endpointGet)
  .catch(
     error => console.log(error)
  )
-
-
-// console.log(allKiller)
-//  const template = generateTemplate (allKiller)
-// setCards(template,$containerCards)
-
-
-// console.log($containerCards.clientHeight)
 
 
 function generateTemplate (array){
@@ -60,8 +65,9 @@ function generateTemplate (array){
 
 
 function createCard (killer){
-    return`
-    <div class="card" id="${killer.name}">
+    let cardClass = isSurvi ? 'card survi' : 'card';
+
+  return  `<div class="${cardClass}" id="${killer.name}">
     <p class="name-killer">${killer.name}</p>
     <figure>
       <img src="./assets/portada/${killer.name}.jpg" alt="" />
@@ -82,7 +88,7 @@ function setCards(template,container){
         container.style.overflow = 'scroll' 
         container.style['overflow-x'] = ' hidden'
     }
-      }, 100);
+      }, 200);
       
     
     console.log(container.clientHeight)
@@ -90,4 +96,71 @@ function setCards(template,container){
 
 }
 
-        // container.style.gap ="5px"
+    function generateTableKiller (killer){
+
+        return`
+        <table class="tabla">
+        <tr>
+          <td class="dato1">name</td>
+          <td class="dato2">${killer.name}</td>
+        </tr>
+        <tr>
+          <td class="dato1">power</td>
+          <td class="dato2">${killer.power}</td>
+        </tr>
+        <tr>
+          <td class="dato1">perk-1</td>
+          <td class="dato2">${killer.perk1}</td>
+        </tr>
+        <tr>
+          <td class="dato1">perk-2</td>
+          <td class="dato2">${killer.perk2}</td>
+        </tr>
+        <tr>
+          <td class="dato1">perk-3</td>
+          <td class="dato2">${killer.perk3}</td>
+        </tr>
+        <tr>
+          <td class="dato1">Movement Speed</td>
+          <td class="dato2">${killer.speed}</td>
+        </tr>
+
+        <tr>
+          <td class="dato1">Terror Radius</td>
+          <td class="dato2">${killer.terrorRadius}</td>
+        </tr>
+        <tr>
+          <td class="dato1">Height</td>
+          <td class="dato2">${killer.height}</td>
+        </tr>
+      </table>`
+        
+    }
+
+
+    function generateTableSurvi (survi){
+
+        return`
+            <table class="tabla">
+        <tr>
+            <td class="dato1">name</td>
+            <td class="dato2">${survi.name}</td>
+        </tr>
+        <tr>
+            <td class="dato1">perk-1</td>
+            <td class="dato2">${survi.perk1}</td>
+        </tr>
+        <tr>
+          <td class="dato1">perk-2</td>
+          <td class="dato2">${survi.perk2}</td>
+      </tr>
+      <tr>
+        <td class="dato1">perk-3</td>
+        <td class="dato2">${survi.perk3}</td>
+    </tr>`
+        
+        
+
+
+
+    }
